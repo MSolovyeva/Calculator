a2b91423804609d9da45309a99f8a2c0b268a7c7
MSolovyeva <ru0483p@gmail.com>
2018-08-08 22:58:24 -0700

    table added

--- scripts.js
+++ scripts.js
@@ -3,59 +3,54 @@ let b = 0;
 let operand = '';
 
 function read() {
-    return document.getElementById('display').value;
+  return document.getElementById('display').value;
 }
-function write(inputValue){
-    document.getElementById('display').value = inputValue
+function write(inputValue) {
+  document.getElementById('display').value = inputValue;
 }
-function substruct(){
-    operand = '-';
+function substruct() {
+  operand = '-';
 }
-function summ(){
-    operand = '+';
+function summ() {
+  operand = '+';
 }
-function divide(){
-    operand = '/';
+function divide() {
+  operand = '/';
 }
-function multiply(){
-    operand = '*';
-}
-
-function getResult(){
-    if(operand === '-'){
-        write(a - b)
-    }else if(operand === '+'){
-        write(+a + +b)
-    }else if(operand === '*'){
-        write(a * b)
-    }else if(operand === '/'){
-        write(a / b)
-    }
-}
-function toInput(inputValue){
-    if(operand && b === 0){
-        write(inputValue);
-        b = inputValue;
-    }else if(operand && b !== 0) {
-        b = read() + inputValue;
-        write(b);
-    } else if (read() === '0'){
-        write('');
-        a = inputValue;
-        write(a);
-    }else if (read() !== '0'){
-        a += inputValue;
-        write(a);
-    }
-}
-function initZero(){
-    write(0);
-    a = 0;
-    b = 0;
-    operand = '';
+function multiply() {
+  operand = '*';
 }
 
-
-
-
-
+function getResult() {
+  if (operand === '-') {
+    write(a - b);
+  } else if (operand === '+') {
+    write(+a + +b);
+  } else if (operand === '*') {
+    write(a * b);
+  } else if (operand === '/') {
+    write(a / b);
+  }
+}
+function toInput(inputValue) {
+  if (operand && b === 0) {
+    write(inputValue);
+    b = inputValue;
+  } else if (operand && b !== 0) {
+    b = read() + inputValue;
+    write(b);
+  } else if (read() === '0') {
+    write('');
+    a = inputValue;
+    write(a);
+  } else if (read() !== '0') {
+    a += inputValue;
+    write(a);
+  }
+}
+function initZero() {
+  write(0);
+  a = 0;
+  b = 0;
+  operand = '';
+}
